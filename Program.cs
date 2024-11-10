e2fd01684d2bf42ff8c919151ad06103c5f3c4e9
FatmaZahra14 <abdultalib.fd739@s.msumain.edu.ph>
2024-11-11 01:30:10 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.Write("What is your Name? ");
+        Console.Write("What is your Name Madam/Sir? ");
         string name = Console.ReadLine();
         Console.WriteLine("Nice to meet you, " + name + "!");
     }
