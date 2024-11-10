a81cbc0f6de9431c8003485a34674397156730a8
FatmaZahra14 <abdultalib.fd739@s.msumain.edu.ph>
2024-11-11 01:33:10 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,13 +7,7 @@ class Program
         Console.Write("What is your Name Madam/Sir? ");
         string name = Console.ReadLine();
         Console.WriteLine("Nice to meet you, " + name + "!");
-        Console.WriteLine("Nice to meet you, " + name + "!");
-        Console.WriteLine("Nice to meet you, " + name + "!");
-
-        Console.WriteLine("Nice to meet you, " + name + "!"); Console.WriteLine("Nice to meet you, " + name + "!");
-
-        Console.WriteLine("Nice to meet you, " + name + "!");
-        Console.WriteLine("Nice to meet you, " + name + "!");
-        Console.WriteLine("Nice to meet you, " + name + "!");
+        COnsole.WriteLine("Just One");
+   
     }
 }
