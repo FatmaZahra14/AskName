f1366aa65085d69e26f6bbff758fa51b9dd2cb18
Fatma Zahra <abdultalib.fd739@s.msumain.edu.ph>
2024-11-11 01:27:46 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Nice to meet you, " + name + "!");
+    }
+}
