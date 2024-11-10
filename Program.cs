640b79dc36905774525497b36375e70935d840bf
Fatma Zahra <abdultalib.fd739@s.msumain.edu.ph>
2024-11-11 01:32:21 +0800

    we have many messages

--- Program.cs
+++ Program.cs
@@ -7,5 +7,13 @@ class Program
         Console.Write("What is your Name Madam/Sir? ");
         string name = Console.ReadLine();
         Console.WriteLine("Nice to meet you, " + name + "!");
+        Console.WriteLine("Nice to meet you, " + name + "!");
+        Console.WriteLine("Nice to meet you, " + name + "!");
+
+        Console.WriteLine("Nice to meet you, " + name + "!"); Console.WriteLine("Nice to meet you, " + name + "!");
+
+        Console.WriteLine("Nice to meet you, " + name + "!");
+        Console.WriteLine("Nice to meet you, " + name + "!");
+        Console.WriteLine("Nice to meet you, " + name + "!");
     }
 }
